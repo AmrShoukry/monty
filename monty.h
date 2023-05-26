306b1ab4f88ed02e16129220bd2d4a220d688fe7
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-26 04:49:16 +0300

    clean files

--- monty.h
+++ monty.h
@@ -22,20 +22,6 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
 void push(stack_t **top, int n, int *mode);
 void pall(stack_t **top);
 void pint(stack_t **top, int i);
