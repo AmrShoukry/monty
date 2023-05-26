2b445fcca7428bb27e23d824ec31c8993cc0a0b6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-26 04:51:03 +0300

    reduce files

--- mandatory3.c
+++ mandatory3.c
@@ -89,3 +89,43 @@ void mod(stack_t **top, int i)
 	pop(top, i);
 }
 
+
+
+/**
+ * rotr - rotate stack to the right
+ *
+ * Description: 'Function'
+ *
+ * @top: top node of the stack
+ *
+ * Return: void.
+ */
+
+
+void rotr(stack_t **top)
+{
+	stack_t *current = *top;
+	stack_t *temp = *top;
+
+	if (current == NULL || current->prev == NULL)
+		return;
+
+	if (current->prev->prev == NULL)
+	{
+		swap(top, 0);
+		return;
+	}
+
+	while (current->prev != NULL)
+	{
+		current = current->prev;
+	}
+
+	temp = current->next;
+	temp->prev = NULL;
+	current->prev = (*top);
+	current->next = NULL;
+	(*top)->next = current;
+
+	(*top) = current;
+}
